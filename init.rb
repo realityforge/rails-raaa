e072fb3e688e0135b6707d9322abadd161bc0bce
Peter Donald <peter@realityforge.org>
2013-04-22 16:38:24 +1000

    Avoid raising an error if testing system is not enabled

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
 require 'authenticated_system'
 ActionController::Base.send :include, AuthenticatedSystem
-Test::Unit::TestCase.send :include, AuthenticatedSystem::Test
+begin
+  Test::Unit::TestCase.send :include, AuthenticatedSystem::Test
+rescue NameError
+  # Don't care if this fails when not testing
+end
